54566c168cf5b432f81cfd3cbb8d1ec985917252
yumion <a.yamada0703@gmail.com>
2019-09-18 22:16:12 +0900

    pyrealsenseをopencvライクに使うクラスを作成

--- wrappers/python/examples/opencv_get_distance.py
+++ wrappers/python/examples/opencv_get_distance.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-import pyrealsense2 as rs
 import cv2
 import numpy as np
+from realsensecv import RealsenseCV
 
 
 # マウスイベント時に処理を行う
@@ -14,42 +14,32 @@ def mouse_event(event, x, y, flags, param):
         return print(distance)
 
 
-# ストリーム(Depth/Color)の設定
-config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-# ストリーミング開始
-pipeline = rs.pipeline()
-profile = pipeline.start(config)
-
+cap = RealsenseCV()
+cap.WIDTH = 640
+cap.HEIGHT = 480
+cap.FPS = 30
+cap.start()
 
 try:
     while True:
-        # フレーム待ち(Color & Depth)
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        depth_frame = frames.get_depth_frame()
-        if not depth_frame or not color_frame:
-            continue
-        # ndarrayに変換
-        color_image = np.asanyarray(color_frame.get_data())
-        depth_image = np.asanyarray(depth_frame.get_data())
+        ret, frames = cap.read()
+        color_frame = frames[0]
+        depth_frame = frames[1]
         # ヒートマップに変換
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
-            depth_image, alpha=0.08), cv2.COLORMAP_JET)
+            depth_frame, alpha=0.08), cv2.COLORMAP_JET)
         # レンダリング
-        images = np.hstack((color_image, depth_colormap))
+        images = np.hstack((color_frame, depth_colormap))
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
 
         # マウスイベント時に関数mouse_eventの処理を行う
-        cv2.setMouseCallback('RealSense', mouse_event, depth_frame)
+        cv2.setMouseCallback('RealSense', mouse_event, cap.depth_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 finally:
     # ストリーミング停止
-    pipeline.stop()
+    cap.stop()
     cv2.destroyAllWindows()
